16b31df861331c158a9e1ee26275754b1a5a4848
Omar Valsson <omar.valsson@gmail.com>
2017-09-21 21:09:42 +0200

    Copyright + headers

--- src/ves/GridProjWeights.h
+++ src/ves/GridProjWeights.h
@@ -4,20 +4,20 @@
 
    See http://www.ves-code.org for more information.
 
-   This file is part of ves-code, version 1.
+   This file is part of ves-code module.
 
-   ves-code is free software: you can redistribute it and/or modify
+   The ves-code module is free software: you can redistribute it and/or modify
    it under the terms of the GNU Lesser General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
-   ves-code is distributed in the hope that it will be useful,
+   The ves-code module is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public License
-   along with ves-code.  If not, see <http://www.gnu.org/licenses/>.
+   along with the ves-code module.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 #ifndef __PLUMED_ves_GridProjWeights_h
 #define __PLUMED_ves_GridProjWeights_h
